0d912b57608cf14091d86dc5491124835b06b3c2
Jaan Aaremäe <aaremae.jaan@gmail.com>
2019-12-25 13:03:40 +0200

    temp 1.7 task unicafe commit

--- part1.6/src/index.js
+++ part1.6/src/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Statistics = ({good, neutral, bad, text}) => {
+  let sum = good + neutral + bad
+  let average = good - bad / (good + neutral + bad)
+  let positive = good / (neutral + bad + good) * 100
+
+  return (
+    <>
+    <p>Sum {sum}</p>
+    <p>Average {average}</p>
+    <p>positive {positive}</p>
+    </>
+  )
+}
+    
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const feedback = [good, neutral, bad]
+console.log(feedback)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <Statistics feedback={feedback} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
