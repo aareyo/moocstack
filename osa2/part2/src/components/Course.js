094df52ca3d0e38ac1c81ff37d1d380770cfd2c0
Jaan Aaremäe <aaremae.jaan@gmail.com>
2019-12-27 00:58:00 +0200

    course component and handling data

---
+++ osa2/part2/src/components/Course.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const Header = ({course}) => {
+    return (
+      <>
+        <h2>{course.name}</h2>
+      </>
+    )
+  }
+
+  const Part = ({part}) => {
+    return (
+    <p>{part.name} {part.exercises}</p>
+    )
+  }
+  
+  const Content = ({course}) => {
+
+    const rows = () => course.parts.map(part =>
+        <Part 
+          key={part.id}
+          part={part}
+        />
+      )
+
+    return (
+      <div>
+        {rows()}
+      </div>
+    );
+  }
+  
+  const Total = ({course}) => {
+
+    const total = 
+    course.parts.map(part => part.exercises).reduce((accumulator, curval) => accumulator + curval)
+  
+    return (
+      <>
+        <p> total of {total} exercises</p>
+      </>  
+      )  
+  }
+
+  const Course = ({course}) => {
+    return (
+        <div>
+          <Header course={course} />
+          <Content course={course} />
+          <Total course={course} />
+        </div>
+      )
+    }
+
+export default Course
--- osa2/part2/src/index.js
+++ osa2/part2/src/index.js
@@ -1,62 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course.name}</h1>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <>
-        {props.course.parts.map((part) => <p>{part.name}</p>)}
-    </>
-  );
-}
-
-const Total = (props) => {
-  let total = 0;
-  
-  props.course.parts.forEach(part => {
-    total += part.exercises;
-  });
-
-  return (
-    <>
-      <p>{total}</p>
-    </>  
-    )  
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
