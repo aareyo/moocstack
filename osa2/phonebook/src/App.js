6f7c3517ba613b44893c32deb99fe1f4b2f2d7fd
Jaan Aaremäe <aaremae.jaan@gmail.com>
2019-12-27 21:57:57 +0200

    Added add name functionality

---
+++ osa2/phonebook/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newPerson, setNewPerson ] = useState('')
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewPerson(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newPerson,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewPerson('')
+  }
+
+  const rows = () => persons.map(person =>
+    <Person
+      key={person.name}
+      person={person}
+    />
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name:
+          <input 
+            value={newPerson}
+            onChange={handlePersonChange}
+          />
+          </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      
+      <h2>Numbers</h2>
+      {rows()}
+    </div>
+  )
+
+}
+
+export default App
