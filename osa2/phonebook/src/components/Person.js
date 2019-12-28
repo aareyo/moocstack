58fd8fdc2f1d670ae5c985cc640a05a3b657676d
Jaan Aaremäe <aaremae.jaan@gmail.com>
2019-12-29 00:18:14 +0200

    Added search filter

--- osa2/phonebook/src/components/Person.js
+++ osa2/phonebook/src/components/Person.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Person = ({person}) => {
-    return (
-<p>{person.name}</p>
-    )
+  return (
+    <div>
+      <p>{person.name} {person.number}</p>
+    </div>
+  )
 }
 
 export default Person
