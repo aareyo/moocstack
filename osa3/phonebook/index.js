a977f2d1fa81652db88ee726e57a9296c41edbac
Jaan Aaremäe <aaremae.jaan@gmail.com>
2020-01-14 22:41:16 +0200

    Tasks up to 3.2

---
+++ osa3/phonebook/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+      { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+      },
+      { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+      },
+      { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+      },
+      { 
+        "name": "Timo Testaaja", 
+        "number": "39-23-64242112",
+        "id": 5
+      }
+    ]
+
+app.get('/info', (request, response) => {
+    response.send(`
+    <div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    </div>
+    `)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.filter(p => p.id === id)
+
+  response.json(person)
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
