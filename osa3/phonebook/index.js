fb4254b77719377294b89bf8ea144f3f6f96b259
Jaan Aaremäe <aaremae.jaan@gmail.com>
2020-01-16 23:29:32 +0200

    Added some validation and return errors

--- osa3/phonebook/index.js
+++ osa3/phonebook/index.js
@@ -1,77 +1,91 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 let persons = [
-      { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-      },
-      { 
-        "name": "Timo Testaaja", 
-        "number": "39-23-64242112",
-        "id": 5
-      }
-    ]
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  },
+  {
+    name: "Timo Testaaja",
+    number: "39-23-64242112",
+    id: 5
+  }
+];
 
-app.get('/info', (request, response) => {
-    response.send(`
+app.get("/info", (request, response) => {
+  response.send(`
     <div>
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date()}</p>
     </div>
-    `)
-})
+    `);
+});
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.filter(p => p.id === id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.filter(p => p.id === id);
 
-  response.json(person)
-})
+  response.json(person);
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(p => p.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-app.post('/api/persons', (request, response) => {
+app.post("/api/persons", (request, response) => {
   // This is bad practice of creating id's, but for tasks sake...
   const randomId = Math.floor(Math.random() * Math.floor(5000000));
-  
-  const person = request.body
-  person.id = randomId
 
-  persons = persons.concat(person)
+  const person = request.body;
+  person.id = randomId;
 
-  response.json(person)
-})
+  const personExists = persons.some(p => p.name === person.name);
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+  if (!person.number) {
+    return response.status(400).json({
+      error: "number missing"
+    });
+  }
+
+  if (personExists) {
+    return response.status(400).json({
+      error: "name must be unique"
+    });
+  }
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const port = 3001;
+app.listen(port);
+console.log(`Server running on port ${port}`);
