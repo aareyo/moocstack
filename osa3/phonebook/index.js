cae6589953a7ffaa0d9f56ebcdd0a583f0faeede
Jaan Aaremäe <aaremae.jaan@gmail.com>
2020-01-17 00:05:35 +0200

    Added morgan middleware

--- osa3/phonebook/index.js
+++ osa3/phonebook/index.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 app.use(bodyParser.json());
+
+morgan.token("body", (req, res) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
